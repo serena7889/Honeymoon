5c0f71759d4314a09248f214ed6a8ab17c8a4e82
Serena <serenalambert1731@gmail.com>
2020-02-05 00:58:33 +0000

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Serena Lambert on 05/02/2020.
+//  Copyright © 2020 Serena Lambert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
